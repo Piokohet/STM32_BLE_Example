d1313d43114bc93474aaafc73a88723410d9dcf5
Piokohet <piokohet@gmail.com>
2022-07-10 13:19:11 +0200

    initial commit of BLE application

---
+++ Core/Inc/app_bluenrg.h
@@ -0,0 +1,14 @@
+/*
+ * app_bluenrg.h
+ *
+ *  Created on: Jul 10, 2022
+ *      Author: sobro
+ */
+
+#ifndef INC_APP_BLUENRG_H_
+#define INC_APP_BLUENRG_H_
+
+void bluenrg_init(void);
+void bluenrg_process(void);
+
+#endif /* INC_APP_BLUENRG_H_ */
